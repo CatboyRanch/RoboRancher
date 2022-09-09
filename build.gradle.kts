220beb67f8d5f87d0abbbe9cf828a55be50e56d3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-09 23:51:06 +0200

    Add gradle bits

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "com.catboyranch"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "com.catboyranch.roborancher.MainKt"
+    }
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        sourceSets.main.get().output
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
